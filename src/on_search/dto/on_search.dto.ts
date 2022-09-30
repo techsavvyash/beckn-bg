dd9a4c9cdc4b1c4158aac602d693514558692844
Yash Mittal <mittal.yash12@hotmail.com>
2022-09-30 12:16:02 +0530

    File: added RatingAck schema
    Signed-off-by: Yash Mittal <mittal.yash12@hotmail.com>

--- src/on_search/dto/on_search.dto.ts
+++ src/on_search/dto/on_search.dto.ts
@@ -1,9 +1,11 @@
 import CatalogInterface from 'src/schemas/Catalog.interface';
 import ContextInterface from 'src/schemas/Context.interface';
+import ErrorInterface from 'src/schemas/Error.interface';
 
 export class OnSearchDTO {
   context: ContextInterface;
-  message: {
+  message?: {
     catalog: CatalogInterface;
   };
+  error?: ErrorInterface;
 }
